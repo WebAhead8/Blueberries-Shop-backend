8df1fabfc3005261ddc059f5d4a0fba38910de46
Ebraheem <ave.brhom@gmail.com>
2021-02-23 12:35:24 +0200

    sql

---
+++ src/database/init.sql
@@ -0,0 +1,39 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, products bucket , productbucket;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  firstName VARCHAR(255),
+  lastName VARCHAR(255),
+  phoneNumber VARCHAR(255),
+  password VARCHAR(255),
+  type VARCHAR(255)
+
+
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  image VARCHAR(255),
+  price NUMBER,
+  quantity NUMBER
+);
+
+CREATE TABLE bucket (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE bucket (
+  id SERIAL PRIMARY KEY,
+  bucket_id INTEGER REFERENCES bucket(id),
+  product_id INTEGER REFERENCES products(id)
+);
+
+
+
+COMMIT;
