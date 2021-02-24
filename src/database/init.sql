f2e1b859365444a66783ecc8a6166653c02854e5
Ebraheem <ave.brhom@gmail.com>
2021-02-24 17:22:51 +0200

    skdhf

--- src/database/init.sql
+++ src/database/init.sql
@@ -35,14 +35,13 @@ CREATE TABLE productBucket (
 );
 
 INSERT INTO products(name,description,price,category,image,quantity)  VALUES
-  ('Bounty','coconut and chocolate ice cream',4,'ice cream','./img/bounty.png',100),
-  ('Cadbury','cramel and milk ice cream, 500 ml',25,'ice cream','./img/cadbury.png',100),
-  ('Cote Dor chocolate','four pcs of luxury chocolate ice cream',45,'ice cream','./img/cote dor chocolate.jpg',100),
-  ('Crunchie','caramel milk and choclolate chips ice cream, 500 ml',25,'ice cream','./img/crunchie.jpg',100),
-  ('Kinder milk chocolate coated',' kinder milk on stick coated with chocolate',4,'ice cream','./img/kinder stick.jpg',100),
-  ('milka sensations','milka sensations bescuits',10,'chocolate','./img/milka sensations.jpg',100),
-  ('kinder cards','kinder cards snacks',4,'chocolate','./img/kinder-cards-snacks1.png',100),
-  ('Coke','coca cola 330ml',5,'drinks','./img/coca-cola-can-034.jpg',100);
+  ('Bounty','coconut and chocolate ice cream',4,'ice cream','/products_img/bounty.png',100),
+  ('Cadbury','cramel and milk ice cream, 500 ml',25,'ice cream','/products_img/cadbury.png',100),
+  ('Crunchie','caramel milk and choclolate chips ice cream, 500 ml',25,'ice cream','/products_img/crunchie.jpg',100),
+  ('Kinder milk chocolate coated',' kinder milk on stick coated with chocolate',4,'ice cream','/products_img/kinder stick.jpg',100),
+  ('milka sensations','milka sensations bescuits',10,'chocolate','/products_img/milka sensations.jpg',100),
+  ('kinder cards','kinder cards snacks',4,'chocolate','/products_img/kinder-cards-snacks1.png',100),
+  ('Coke','coca cola 330ml',5,'drinks','/products_img/coca-cola-can-034.jpg',100);
   
     
  INSERT INTO users(email,firstName,lastName,phoneNumber,password,type) VALUES 
