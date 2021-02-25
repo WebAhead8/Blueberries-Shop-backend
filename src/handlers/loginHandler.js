1992a7f2f1f434a6a7607e6eb61293171ee58fa7
Ebraheem <ave.brhom@gmail.com>
2021-02-25 16:12:30 +0200

    ksjd

--- src/handlers/loginHandler.js
+++ src/handlers/loginHandler.js
@@ -6,12 +6,12 @@ function login(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
     console.log(process.env.JWT_SECRET);
-    console.log("lsdfnsdlkgnksjdnfkjsdnskjnfskdjnfdsjkn");
+
 
     model.getUser(email, password).then(result => {
         if (result) {
 
-            const token = jwt.sign({ user: result.id }, "lskfnlsdkn", { expiresIn: "1h" });
+            const token = jwt.sign({ user: result.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
             res.status(200).send({ access_token: token })
         } else {
             const error = new Error("no user Found");
